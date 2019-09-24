754ca8c51ba4d39b61b6959e288f99fab60a5fd0
Suwardhana <spensajambi@gmail.com>
2019-09-24 13:52:27 +0700

    info badge

--- lib/mypoint.dart
+++ lib/mypoint.dart
@@ -13,7 +13,7 @@ class _MyPoinState extends State<MyPoin> {
   Widget build(BuildContext context) {
     return Container(
       child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
@@ -44,7 +44,20 @@ class _MyPoinState extends State<MyPoin> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   myPointWidget(),
-                ])
+                ]),
+            Column(
+              children: <Widget>[
+                _badgeImage2(1),
+                _badgeImage2(2),
+                _badgeImage2(3),
+                _badgeImage2(4),
+                _badgeImage2(5),
+                _badgeImage2(6),
+                _badgeImage2(7),
+                _badgeImage2(8),
+                _badgeImage2(9),
+              ],
+            ),
           ]),
     );
   }
@@ -54,7 +67,7 @@ class _MyPoinState extends State<MyPoin> {
     return Text(
       myPoint,
       style: TextStyle(
-          fontSize: 29.0,
+          fontSize: 40.0,
           color: const Color(0xFF000000),
           fontWeight: FontWeight.w200,
           fontFamily: "Roboto"),
@@ -74,7 +87,7 @@ class _MyPoinState extends State<MyPoin> {
     }
     return Image.asset(
       'badge/lvl' + level.toString() + '.png',
-      width: 50.0,
+      width: 25.0,
     );
   }
 
@@ -85,4 +98,28 @@ class _MyPoinState extends State<MyPoin> {
       myPoint = _point.toString();
     });
   }
+}
+
+Widget _badgeImage2(int level) {
+  var v = ((level) * 10);
+  var v2 = v + 9;
+  String v3 = v.toString() + " - " + v2.toString();
+  if (level < 2) {
+    v3 = "1 - 19";
+  }
+  if (level > 8) {
+    v3 = " > 90";
+  }
+  return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Image.asset(
+          'badge/lvl' + level.toString() + '.png',
+          width: 25.0,
+        ),
+        Text("Level $level"),
+        Text(v3),
+      ]);
 }
